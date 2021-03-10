4301f234528981cdf5c7a8fd09f49aa2edce5fda
arunas445 <skittlessxit@gmail.com>
2021-03-10 22:37:15 +0200

    added one view to show all blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    blog_title = Post.objects.all().order_by('-published_date')
+    context = {
+        'i': blog_title
+    }
+    return render(request, 'blog/post_list.html', context)
